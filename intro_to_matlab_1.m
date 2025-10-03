ae601c889a5b5e9b5c7f78e4c888c3842419cb44
Robert Bain <31903812+bainro@users.noreply.github.com>
2025-10-03 00:39:04 -0700

    Update intro_to_matlab_1.m

--- intro_to_matlab_1.m
+++ intro_to_matlab_1.m
@@ -1,28 +1,22 @@
-% unnecessary; clears previous output
-clear all; close all; clc;
+% unnecessary for small scripts; clears previous output and windows
+clear all; close all; clc;   % clear vars, close figs, clear command window
 
 %% Free, official matlab demos:
 % Just type "demos" into the Command Window.
 
 %% COMMENTS
-
 % <-- comments in matlab start with this symbol
 % everything after '%' on this line is ignored. 
-
 %{
-
 This is a multi-line comment.
 So this line is ignored by matlab too.
 And this! Etc...
-
 COMMENT YOUR CODE! Especially the hard parts.
 Your future self and anyone else who reads 
 your code will thank you! *cough* Bruce *cough*
-
 %}
 
 %% BASIC SYNTAX (think grammar for nerds)
-
 5
 5 + 2
 5 * 2
@@ -33,13 +27,13 @@ your code will thank you! *cough* Bruce *cough*
 4 ^ 3;
 
 %% STRINGS (represent text rather than numbers)
-
+% ⚠️ IMPORTANT: double quotes " " create modern string objects.
+% Single quotes ' ' create character arrays, which behave differently.
 "hello world";
 "mom get the camera, I'm coding!";
 "👁️❤️️🎃☕";
 
 %% ASSIGNING RESULTS TO VARIABLES
-
 % we can save our calculations into variables:
 my_1st_var = "✊✋️✌️";
 % other languages call this a matrix, list, row_vector, etc.
@@ -50,49 +44,40 @@ disp(my_1st_array)
 disp("the 1st element of my array is: " + my_1st_array(1))
 
 %% USING BUILT-IN MATLAB FUNCTIONS
-
-% 5 divided by 2 and throw out the remainder/decimal
-ceil_div_ans = ceilDiv(5, 2);
+% Functions take inputs inside parentheses and return outputs
+ceil_div_ans = ceil(5/2);   % round 5/2 up
 disp("ceil: " + ceil_div_ans);
-% same thing but round up to the next whole number
-floor_div_ans = floorDiv(5, 2);
+floor_div_ans = floor(5/2); % round 5/2 down
 disp("floor: " + floor_div_ans);
 
 %% IF / ELSE STATEMENTS
-
 you_study = true;
-
 if you_study
     your_grade = "good";
 end
-
-if not(you_study)
+if not(you_study)   % 'not' can also be written as '~'
     your_grade = "not so good";
 end
-
 disp("your grade is " + your_grade)
 
 you_study = false;
-
 if you_study
     your_grade = "good";
 else
     your_grade = "not so good";
 end
-
 disp("your grade is " + your_grade)
 
 % rock, paper, scissors example of "elseif"
-
 choices = ["✊", "✋️", "✌️"];
 % what's our choice?
 our_choice = choices(3);
 number_of_choices = length(choices);
 random_element_id = randi(number_of_choices);
 they_choose = choices(random_element_id);
-
 % outcome based on your opponent's random choice
-% '==' is checking if the two are equal, returning true of false
+% '==' checks equality. Works with string objects " ".
+% For character arrays ' ' you would instead use strcmp().
 if they_choose == "✊"
     disp(":(") % you lose
 elseif they_choose == "✋️"
@@ -102,41 +87,35 @@ else
 end
 
 %% FOR LOOPS
-
 % another way to create an array
 one_thru_ten = 1:1:10; 
 % we'll use this variable to add up the array elements
 running_sum = 0;
-
-% @TODO expand this out first to show the silly way
 for individual_array_element = one_thru_ten
   running_sum = running_sum + individual_array_element;
 end
-
 disp("the sum of 1-10 is: " + running_sum)
 
 %% MATRICES
-
 % Anything in square brackets is a matrix
 row_vector = [1 2 3 4 5 6]
 col_vector = transpose(row_vector)
 two_by_three_matrix = [1 2 3; 4 5 6]
 % ' is the transpose operator, a shortcut for the transpose function
 three_by_two_matrix = two_by_three_matrix'
-% use indices inside parenthesis to get a specific value from a matrix:
+% MATLAB indexing is row, column (like math notation):
 disp("first row, second column of matrix: " + two_by_three_matrix(1, 2))
-% use the colon operate obtain multiple values:
+% use the colon operator to obtain multiple values:
 two_by_three_matrix(1, 1:3)
-% colon by itself to implicitally grab 'all' values from the row
+% colon by itself to grab 'all' values along that dimension:
 two_by_three_matrix(2, :)
-
 % Be careful! Matrix operators often have two forms: 
 % matrix algebra form (*, / and ^) and element-wise form (.*, ./ and .^)
+[1 2 3] .* [4 5 6]   % element-wise multiply
+[1 2 3] * [4; 5; 6]  % matrix multiply
 
 %% WRITING FUNCTIONS (must be at the end of the file)
-
 disp("sum of 49, 50, and 51: " + sum_array(49, 51))
-
 % SYNTAX: function returned_value = fx_name([var1, var2, ...])
 function running_sum = sum_array(start_n, end_n)
     % sums whole numbers between function argument 1 and 2
